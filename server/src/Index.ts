d0be2c1559374f11c8f7dd4669fe98be2f9cb3a1
Febzey <braydent1235@gmail.com>
2022-03-22 18:08:53 -0500

    added server

---
+++ server/src/Index.ts
@@ -0,0 +1,30 @@
+import 'dotenv/config'
+import fastify       from 'fastify';
+import path          from 'path';
+import fastifyStatic from 'fastify-static';
+
+const server = fastify({ logger: false });
+
+console.log(__dirname)
+const webPath = path.join(__dirname, '../../dist');
+const options = {
+    root: webPath,
+    prefix: "/*"
+}
+
+server.register(fastifyStatic, options);
+
+server.setNotFoundHandler((request, reply) => {
+    return reply.send({ Error: "Page not found."})
+});
+
+
+(async () => {
+    try {
+        await server.listen(process.env.port)
+        console.log(`Server listening on port ${process.env.port}`)
+    } catch (err) {
+        server.log.error(err);
+        process.exit(1)
+    }
+})();
